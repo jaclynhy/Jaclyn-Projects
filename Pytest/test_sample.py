efe27d2f8c145956c94f8c5e1051014bd5557b0f
jaclynhy <jaclynhy@gmail.com>
2026-04-14 17:43:42 +0800

    test script using comma

--- Pytest/test_sample.py
+++ Pytest/test_sample.py
@@ -1,1 +1,8 @@
-print('Hello, World!')
+print('Hello, World! What\'s your name?')
+name = input()
+print(f'Nice to meet you, {name}!')
+print('How did you get here?')
+
+#Test script using comma for a new line
+print ('Hello,'),
+print ('world!')
